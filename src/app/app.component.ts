734b894dbe6a877a7ad7c4075a5a1f8a47f6a591
jay910817 <106866435+jay910817@users.noreply.github.com>
2023-08-07 14:37:00 +0800

    html has a error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,19 +4,36 @@ import { ToDoService } from './to-do.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'todo-list';
+
+  todoList:any[] = [];
+
+  constructor(public toDoService: ToDoService) {}
+  ngOnInit(): void {
+    this.todoList = this.toDoService.getTODOList();
+    console.log("todo list:", this.todoList);
+  }
+
+  //紀錄最新id
+  addId = 0;
+  addTODOTitle = "";
+
+  addTODO(){
+    //輸入長度為0不新增
+    if(this.addTODOTitle.length==0)return;
+
+    //增加一筆代辦事項
+    this.todoList.push({
+      id:this.addId,
+      todo:this.addTODOTitle
+    });
+
+    //流水號增加
+    this.addId++;
+    this.addTODOTitle = "";
+  }
+
 }
-
-todoList = [];
-
-constructor(public toDoService: ToDoService){
-
-}
-ngOnInit(): void {
-  this.todoList = this.toDoService.getTODOList();
-  console.log("todo list:", this.todoList);
-}
-
